fc411a7983ae3d0c4c3ccc8b426459fddf754638
AlinaGay <gayfullina.a@gmail.com>
2024-09-10 19:27:37 +0200

    add cipher_master

--- fisrt_sprint/refrigerator.py
+++ fisrt_sprint/refrigerator.py
@@ -13,76 +13,76 @@ goods = {
         {'amount': Decimal('1.5'), 'expiration_date': None}
     ],
 }
+
+
 def add(items, title, amount, expiration_date=None):
     if expiration_date is not None:
-       #expiration_date= datetime.strptime(expiration_date, '%Y-%m-%d')
-       expiration_date= datetime.strptime(expiration_date, '%Y-%m-%d').date()
+        # expiration_date= datetime.strptime(expiration_date, '%Y-%m-%d')
+        
+        expiration_date= datetime.strptime(expiration_date, '%Y-%m-%d').date()
     if items.get(title) == None:
-       items[title] = [{'amount': amount, 'expiration_date': expiration_date}]     
+        items[title] = [{'amount': amount, 'expiration_date': expiration_date}]     
     else:
-       items[title].append({'amount': amount, 'expiration_date': expiration_date})
+        items[title].append({'amount': amount, 'expiration_date': expiration_date})
 
 
 def add_by_note(items, note):
     note_list = list(note.split(" "))
     if '-' in note_list[-1]:
-       expiration_date = note_list[-1]
-       amount = Decimal(note_list[-2])
-       title = ' '.join(note_list[:-2])
-       add(items, title, amount, expiration_date)
+        expiration_date = note_list[-1]
+        amount = Decimal(note_list[-2])
+        title = ' '.join(note_list[:-2])
+        add(items, title, amount, expiration_date)
     else:
         amount = Decimal(note_list[-1])
         title = ' '.join(note_list[:-1])
         add(items, title, amount)
 
+
 def find(items, needle):
-   needle_list = []
-   for title in items.keys():
-      if needle.lower() in title.lower():
-         needle_list.append(title)
-   return needle_list
+    needle_list = []
+    for title in items.keys():
+        if needle.lower() in title.lower():
+            needle_list.append(title)
+    return needle_list
 
 
 def amount(items, needle):
-   total_amount = Decimal('0')
-   for title, value in items.items():
-      if needle.lower() in title.lower():
-         count = len(value)
-         for i in range(count):
-            total_amount += value[i]['amount']
-      
-   
-   return total_amount   
+    total_amount = Decimal('0')
+    for title, value in items.items():
+        if needle.lower() in title.lower():
+            count = len(value)
+            for i in range(count):
+                total_amount += value[i]['amount']
+    return total_amount
 
 
 def expire(items, in_advance_days=0):
-    
     today = datetime.today().date()
     expired_product_list = []
 
     if in_advance_days == None:
         in_advance_days = 0
-        
     for title, value in items.items():
-      count = len(value)
-      total_amount = 0
-      for i in range(count):
-         if value[i]['expiration_date'] != None:
-            difference = (value[i]['expiration_date'] - today).days
-            if difference <= in_advance_days:
-                if expired_product_list != []:
-                    if expired_product_list[-1][0] == title:
-                        expired_product_list[-1][-1] += value[i]['amount']
-                        print(expired_product_list[-1][0])
+        count = len(value)
+        for i in range(count):
+            if value[i]['expiration_date'] != None:
+                difference = (value[i]['expiration_date'] - today).days
+                if difference <= in_advance_days:
+                    if expired_product_list != []:
+                        if expired_product_list[-1][0] == title:
+                            expired_product_list[-1][-1] += value[i]['amount']
+                            print(expired_product_list[-1][0])
+                        else:
+                            expired_product = [title, value[i]['amount']]
+                            expired_product_list.append(expired_product)    
+        
                     else:
                         expired_product = [title, value[i]['amount']]
-                        expired_product_list.append(expired_product)    
-       
-                else:
-                    expired_product = [title, value[i]['amount']]
-                    expired_product_list.append(expired_product)
-    expired_product_tuple_list = [tuple(expired_product)  for expired_product in expired_product_list]                   
+                        expired_product_list.append(expired_product)
+    expired_product_tuple_list = [tuple(expired_product) for expired_product in expired_product_list]                   
     return expired_product_tuple_list
+
 
 add(goods, 'Молоко', Decimal('1.6'), '2024-10-28')
 print(goods) 
---
+++ second_sprint/cipher_master.py
@@ -0,0 +1,50 @@
+class CipherMaster:
+    alphabet = 'абвгдеёжзийклмнопрстуфхцчшщъыьэюя'
+
+    def cipher(self, original_text, shift):
+        original_text = list(original_text.split(" "))
+        ciphered_text = []
+        for word in original_text:
+            ciphered_word = ''
+            for char in word:
+                char = char.lower()
+                if char in [',', '.', '!', ':', ';', '"', '(', ')']:
+                    shifted_letter = char
+                else:
+                    shifted_letter_position = self.alphabet.index(char) + shift
+                    if shifted_letter_position % len(self.alphabet) > 0:
+                        shifted_letter_position = shifted_letter_position % len(self.alphabet)
+                    shifted_letter = self.alphabet[shifted_letter_position]
+                print('char', char, 'shifted_letter', shifted_letter)    
+                ciphered_word += shifted_letter
+            ciphered_text.append(ciphered_word)
+        return ' '.join(ciphered_text)
+
+    def decipher(self, cipher_text, shift):
+        cipher_text = list(cipher_text.split(" "))
+        original_text = []
+        for word in cipher_text:
+            original_word = ''
+            for char in word:
+                char = char.lower()
+                if char in [',', '.', '!', ':', ';', '"', '(', ')', '—']:
+                    original_letter = char
+                else:
+                    
+                    original_letter_position = self.alphabet.index(char) + shift
+                    original_letter = self.alphabet[original_letter_position]
+                    #print('char', char, 'original_letter', original_letter)
+                original_word += original_letter
+            original_text.append(original_word)
+        return ' '.join(original_text)
+
+
+cipher_master = CipherMaster()
+print(cipher_master.cipher(
+    original_text='Однажды ревьюер принял проект с первого раза, с тех пор я его боюсь',
+    shift=2
+))
+print(cipher_master.decipher(
+    cipher_text='Олебэи яфвнэ мроплж сэжи — э пэй рдв злййвкпш лп нвящывнэ',
+    shift=-3
+))
